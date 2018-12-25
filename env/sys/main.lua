d14ee92be27db98601f3bfe50fb32452eff06cef
SJY2016 <1433653386@qq.com>
2018-12-25 21:03:55 +0800

    update

--- env/sys/main.lua
+++ env/sys/main.lua
@@ -1,22 +1,43 @@
 
-require"sys.msg";
-local leftDlg = require 'sys.leftDlg'
+
 ------------------------------------------------------------------------------
+require"sys.msg.on_sys_msg";
+------------------------------------------------------------------------------
+CODE  =  require 'sys.code'
+TOOLS = require 'sys.tools'
+IUP = require 'sys.iup'
+DISK = require 'sys.disk'
+ZIP = require 'sys.zip'
+
+SETTING =  require 'sys.setting'
+KEYWORD =  require 'sys.keyword'
+LANG = require 'sys.lang'
+PAGE = require 'sys.page'
+
+
+------------------------------------------------------------------------------
+local _M = {}
+local modname = ...
+_G[modname] = _M
+package.loaded[modname] = _M
+
+local setting = SETTING
+local sysPage = PAGE
+local sysKeyword = KEYWORD
+local sysLang = LANG
+
+_ENV = _M
 
 function load()
-	leftDlg.load()
+	sysPage.init()
+	setting.init()
+	sysPage.update()
+	-- browser.load()
+	setting.update()
 end
 
-function init()
-	leftDlg.init()
-end
-
-function open()
-	
-end
 
 load();
-init();
 
 
 
